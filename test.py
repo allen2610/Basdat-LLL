81f804923096688f6cae7a6ed18022b0e29fb3f2
4clarissaNT4 <166925249+ClarissaNT44@users.noreply.github.com>
2025-05-09 19:53:25 +0700

    test

---
+++ test.py
@@ -0,0 +1,19 @@
+import mariadb
+
+with open("Patra.sql", "r") as file:
+    query = file.read()
+
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="password_sendiri",
+        host="localhost",
+        port=3306
+    )
+    cur = conn.cursor()
+    cur.execute(query)
+    print("Database berhasil dibuat dari file SQL.")
+except mariadb.Error as e:
+    print(f"Error: {e}")
+finally:
+    conn.close()
