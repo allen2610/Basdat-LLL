846698228384a247efa6abef7ce4110cccb6f45d
William <115348219+Azekhiel@users.noreply.github.com>
2025-05-16 11:33:10 +0700

    Update subscribe.sql

--- subscribe.sql
+++ subscribe.sql
@@ -8,9 +8,9 @@ CREATE TABLE Subscribe (
     jumlah         INT NOT NULL,
     tanggal_bayar  DATE NOT NULL,
     PRIMARY KEY (id_supporter, id_creator, nama_tier),
-    FOREIGN KEY (id_supporter)  REFERENCES Suporter(id_suporter),
-    FOREIGN KEY (id_creator)    REFERENCES Tier(id_creator),
-    FOREIGN KEY (nama_tier)     REFERENCES Tier(nama),
+    FOREIGN KEY (id_supporter)  REFERENCES Suporter(id_suporter) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_creator)    REFERENCES Tier(id_creator) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (nama_tier)     REFERENCES Tier(nama) ON DELETE CASCADE ON UPDATE CASCADE,
     CHECK (tanggal_bayar >= tanggal_mulai)
 );
 
